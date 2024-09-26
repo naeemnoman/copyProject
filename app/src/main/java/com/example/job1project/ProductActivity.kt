f7b0df805a45cb150000deb312cf98eda8bbc359
naeemnoman <naeemnoman0419@gmail.com>
2024-09-26 13:50:55 +0600

    done

--- app/src/main/java/com/example/job1project/ProductActivity.kt
+++ app/src/main/java/com/example/job1project/ProductActivity.kt
@@ -1,20 +1,45 @@
 package com.example.job1project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.job1project.Adapter.ProdutAdapter
+import com.example.job1project.ViewModel.ProductViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProductActivity : AppCompatActivity() {
+    private lateinit var productViewModel: ProductViewModel
+    private lateinit var productAdapter: ProdutAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_product)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        productViewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
+        productAdapter = ProdutAdapter(emptyList())
+
+        val refreshBtn:FloatingActionButton = findViewById(R.id.refreshBtn)
+        refreshBtn.setOnClickListener {
+            startActivity(Intent(this@ProductActivity, MainActivity::class.java))
+            finish()
         }
+
+        val recyclerView : RecyclerView = findViewById(R.id.productRv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        productViewModel.products.observe(this) { products ->
+            productAdapter = ProdutAdapter(products)
+            recyclerView.adapter = productAdapter
+
+
+
+        }
+
     }
 }
